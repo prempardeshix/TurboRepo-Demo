c44345528d222361a858e87adeddf0eaaf529121
Prem Pardeshi <prempardeshi283@gmail.com>
2024-07-19 21:07:08 +0530

    Final commit

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,10 @@
-import express from "express"; 
-const app = express();
+import express from "express";
 
+import { BACKEND_URL } from "@repo/common/config";
+
+console.log(BACKEND_URL);
+
+const app = express();
 const port: number = 3000;
 
 app.get("/", (req, res) => {
